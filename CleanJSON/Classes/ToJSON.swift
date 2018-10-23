0a67b72f598e836356cf6217f67af4a8c1643564
Pircate <gao497868860@163.com>
2018-10-23 18:34:40 +0800

    add toJSON

---
+++ CleanJSON/Classes/ToJSON.swift
@@ -0,0 +1,18 @@
+//
+//  ToJSON.swift
+//  CleanJSON
+//
+//  Created by Pircate(gao497868860@gmail.com) on 2018/10/23
+//  Copyright © 2018 Pircate. All rights reserved.
+//
+
+public extension Encodable {
+    
+    func toJSON(using encoder: JSONEncoder = .init()) throws -> Data {
+        return try encoder.encode(self)
+    }
+    
+    func toJSONString(using encoder: JSONEncoder = .init()) throws -> String? {
+        return String(data: try toJSON(using: encoder), encoding: .utf8)
+    }
+}
