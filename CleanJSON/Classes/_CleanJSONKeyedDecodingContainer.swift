0771656bc26fd5812e1893206269ad2d772c49f1
Meow <gao497868860@gmail.com>
2019-03-26 15:22:35 +0800

    Update to Swift 5.0

--- CleanJSON/Classes/_CleanJSONKeyedDecodingContainer.swift
+++ CleanJSON/Classes/_CleanJSONKeyedDecodingContainer.swift
@@ -490,7 +490,7 @@ private extension CleanJSONDecoder.KeyDecodingStrategy {
         guard !stringKey.isEmpty else { return stringKey }
         
         // Find the first non-underscore character
-        guard let firstNonUnderscore = stringKey.index(where: { $0 != "_" }) else {
+        guard let firstNonUnderscore = stringKey.firstIndex(where: { $0 != "_" }) else {
             // Reached the end without finding an _
             return stringKey
         }
