f0b4689bb6dd6b8a88ce83ea5da49acb70f987df
Pircate <gao497868860@gmail.com>
2020-08-26 18:22:50 +0800

    ref | 限制解析的数据类型

--- Sources/CleanJSON/CleanJSONDecoder.swift
+++ Sources/CleanJSON/CleanJSONDecoder.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+public protocol JSONContainerConvertible {
+    func asContainer() -> Any
+}
+
+extension Dictionary: JSONContainerConvertible where Key == String, Value == Any {
+    public func asContainer() -> Any { self }
+}
+
+extension Array: JSONContainerConvertible where Element == Any {
+    public func asContainer() -> Any { self }
+}
+
 open class CleanJSONDecoder: JSONDecoder {
     
     /// Options set on the top-level encoder to pass down the decoding hierarchy.
@@ -73,17 +85,31 @@ open class CleanJSONDecoder: JSONDecoder {
             )
         }
         
-        return try decode(type, from: topLevel)
+        let decoder = _CleanJSONDecoder(referencing: topLevel, options: self.options)
+        guard let value = try decoder.unbox(topLevel, as: type) else {
+            throw DecodingError.valueNotFound(
+                type, DecodingError.Context(
+                    codingPath: [],
+                    debugDescription: "The given data did not contain a top-level value."
+                )
+            )
+        }
+        
+        return value
     }
     
     /// Decodes a top-level value of the given type from the given JSON representation.
     ///
     /// - parameter type: The type of the value to decode.
-    /// - parameter container: The container to decode from.
+    /// - parameter convertible: The container to decode from.
     /// - returns: A value of the requested type.
     /// - throws: `DecodingError.dataCorrupted` if values requested from the payload are corrupted.
     /// - throws: An error if any value throws an error during decoding.
-    open func decode<T : Decodable>(_ type: T.Type, from container: Any) throws -> T {
+    open func decode<T : Decodable>(
+        _ type: T.Type,
+        from convertible: JSONContainerConvertible
+    ) throws -> T {
+        let container = convertible.asContainer()
         let decoder = _CleanJSONDecoder(referencing: container, options: self.options)
         
         guard let value = try decoder.unbox(container, as: type) else {
