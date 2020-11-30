22273a2b4edd935eb72e348ae459ddb411d4abcc
Pircate <gao497868860@gmail.com>
2020-11-30 22:15:08 +0800

    build: 支持 xcframework

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CleanJSON",
-            targets: ["CleanJSON"]),
+            targets: ["CleanJSON"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +19,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "CleanJSON",
-            dependencies: []),
+        .binaryTarget(name: "CleanJSON", path: "CleanJSON.xcframework"),
         .testTarget(
             name: "CleanJSONTests",
-            dependencies: ["CleanJSON"]),
+            dependencies: ["CleanJSON"]
+        ),
     ]
 )
